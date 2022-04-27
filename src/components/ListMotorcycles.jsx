0f5568bc15fc9df38492108eaea7dd4a79e0eddd
nielsdylan <niels_dylan@hotmail.com>
2022-04-26 22:34:58 -0500

    back completo a inicios del uso del axios

--- src/components/ListMotorcycles.jsx
+++ src/components/ListMotorcycles.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import axios from 'axios';
 
 const ListMotorcycles = () => {
+  const URL = "http://127.0.0.1:8000/api/motos";
+  axios.get(URL);
+
   return (
     <div>ListMotorcycles</div>
   )
